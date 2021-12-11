97418226f876acf8e1d33af97c6e093cdcdcd035
Emerson Vieira <33058881+emevieira123@users.noreply.github.com>
2021-12-10 22:25:22 -0400

    Implementando o Modal e Fomrs

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import { App } from './App';
 
 createServer({
-  routes() {
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-02-12 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Curso Rocketseat',
+          type: 'withdraw',
+          category: 'Compras Web',
+          amount: 800,
+          createdAt: new Date('2021-02-23 14:00:00'),
+        }
+      ],
+    })
+  },
+
+  routes() {  
+
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction',
-          amount: 400,
-          type: 'deposit',
-          cotegory: 'Food',
-          createAt: new Date()
-        }
-      ]
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data);
     })
   }
 })
